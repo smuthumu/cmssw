78c509e462c52ba9ac78dff18dd57c3f278e4d9d
Kevin Pedro <kpedro88@gmail.com>
2016-08-02 13:09:16 -0500

    customize digi collection for hcal validation

--- SLHCUpgradeSimulations/Configuration/python/HCalCustoms.py
+++ SLHCUpgradeSimulations/Configuration/python/HCalCustoms.py
@@ -88,9 +88,11 @@ def customise_Hcal2017(process):
     if hasattr(process,'datamixing_step'):
         process=customise_mixing(process)
     if hasattr(process,'dqmoffline_step'):
-        process.digiTask.tagHBHE = cms.untracked.InputTag("simHcalDigis")    
-        process.digiTask.tagHF = cms.untracked.InputTag("simHcalDigis")    
-        process.digiTask.tagHO = cms.untracked.InputTag("simHcalDigis")    
+        process.digiTask.tagHBHE = cms.untracked.InputTag("simHcalDigis")
+        process.digiTask.tagHF = cms.untracked.InputTag("simHcalDigis")
+        process.digiTask.tagHO = cms.untracked.InputTag("simHcalDigis")
+    if hasattr(process,'validation_step'):
+        process.AllHcalDigisValidation.digiLabel = cms.InputTag("simHcalDigis")
 
     return process
     
