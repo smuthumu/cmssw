1c4c1b66c65e3fd58668620a3d98da90b43bbd46
Kevin Pedro <kpedro88@gmail.com>
2016-10-25 16:40:59 -0500

    introduce run3_HB Era

--- Configuration/StandardSequences/python/Eras.py
+++ Configuration/StandardSequences/python/Eras.py
@@ -29,6 +29,7 @@ class Eras (object):
                            'stage1L1Trigger', 'fastSim',
                            'run2_HE_2017', 'stage2L1Trigger',
                            'run2_HF_2017', 'run2_HCAL_2017',
+                           'run3_HB',
                            'phase1Pixel', 'run3_GEM',
                            'phase2_common', 'phase2_tracker',
                            'phase2_hgcal', 'phase2_muon', 'phase2_timing',
