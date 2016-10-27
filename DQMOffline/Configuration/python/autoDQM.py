0326b3f4fd887eb5c574b5ead0837f48729eee70
Kevin Pedro <kpedro88@gmail.com>
2016-10-27 11:32:28 -0500

    additional HCAL DQM sequence for Phase2

--- DQMOffline/Configuration/python/autoDQM.py
+++ DQMOffline/Configuration/python/autoDQM.py
@@ -13,6 +13,9 @@ autoDQM = { 'common' : ['DQMOfflineCommon',
             'hcal':     ['DQMOfflineHcal',
                          'PostDQMOffline',
                          'DQMHarvestHcal'],
+            'hcal2': ['HcalDQMOfflineSequence',
+                      'PostDQMOffline',
+                      'HcalDQMOfflinePostProcessor'],
             'jetmet':  ['DQMOfflineJetMET',
                         'PostDQMOffline',
                         'DQMHarvestJetMET+DQMCertJetMET'],
@@ -49,7 +52,8 @@ autoDQM = { 'common' : ['DQMOfflineCommon',
                           'dqmHarvesting']
             }
 
-_phase2_allowed = ['trackingOnlyDQM','muon','hcal']
+_phase2_allowed = ['trackingOnlyDQM','muon','hcal','hcal2']
 autoDQM['phase2'] = ['','','']
-for i in range(0,3):
+for i in [0,2]:
     autoDQM['phase2'][i] = '+'.join([autoDQM[m][i] for m in _phase2_allowed])
+autoDQM['phase2'][1] = 'PostDQMOffline'
