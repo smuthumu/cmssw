e8766ed96a9788cd6c32a95e89cd9ee43b686c6d
Kevin Pedro <kpedro88@gmail.com>
2017-03-22 17:07:29 -0500

    protect against inexplicable crash in RecoTauPiZeroStripPlugin3

--- PhysicsTools/CandUtils/src/AddFourMomenta.cc
+++ PhysicsTools/CandUtils/src/AddFourMomenta.cc
@@ -6,8 +6,9 @@ using namespace reco;
 void AddFourMomenta::set( Candidate & c ) const {
   Candidate::LorentzVector p4( 0, 0, 0, 0 );
   Candidate::Charge charge = 0;
-  Candidate::iterator b = c.begin(), e = c.end(); 
-  for(  Candidate::iterator d = b; d != e; ++ d ) {
+  for( unsigned id = 0; id < c.numberOfDaughters(); ++id){
+    const Candidate* d = c.daughter(id);
+    if(!d) continue;
     p4 += d->p4();
     charge += d->charge();
   }
