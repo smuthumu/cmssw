4381aac8b64bc3b1631197f299bef97ec8eee379
Kevin Pedro <kpedro88@gmail.com>
2017-07-13 16:36:26 -0500

    reduce modification frequency of Eras.py

---
+++ Configuration/StandardSequences/python/ErasList.py
@@ -0,0 +1,42 @@
+import FWCore.ParameterSet.Config as cms
+
+allEras = ['Run1_pA',
+           'Run1_peripheralPbPb',
+           'Run2_50ns',
+           'Run2_50ns_HIPM',
+           'Run2_25ns',
+           'Run2_25ns_HIPM',
+           'Run2_25ns_peripheralPbPb',
+           'Run2_HI',
+           'Run2_2016',
+           'Run2_2016_HIPM',
+           'Run2_2016_trackingLowPU',
+           'Run2_2016_pA',
+           'Run2_2017',
+           'Run2_2017_trackingRun2',
+           'Run2_2017_trackingPhase1QuadProp',
+           'Run2_2017_trackingLowPU',
+           'Run2_2018',
+           'Run3',
+           'Phase2',
+           'Phase2_timing',
+           'Phase2_timing_layer',
+]
+
+internalUseMods = ['run2_common', 'run2_25ns_specific',
+                   'run2_50ns_specific', 'run2_HI_specific',
+                   'stage1L1Trigger', 'fastSim',
+                   'peripheralPbPb', 'pA_2016',
+                   'run2_HE_2017', 'stage2L1Trigger',
+                   'run2_HF_2017', 'run2_HCAL_2017', 'run2_HEPlan1_2017',
+                   'run3_HB',
+                   'phase1Pixel', 'run3_GEM', 'run2_GEM_2017', 'run2_GEM_2017_MCTest',
+                   'phase2_common', 'phase2_tracker',
+                   'phase2_hgcal', 'phase2_muon', 'phase2_timing',
+                   'phase2_timing_layer','phase2_hcal',
+                   'trackingLowPU', 'trackingPhase1', 'trackingPhase1QuadProp', 'ctpps_2016', 'trackingPhase2PU140',
+                   'tracker_apv_vfp30_2016', 'run2_miniAOD_80XLegacy',
+]
+
+internalUseModChains = ['run2_2017_noTrackingModifier',
+]
