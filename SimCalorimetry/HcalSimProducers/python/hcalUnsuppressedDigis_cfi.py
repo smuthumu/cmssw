a151759c53a85eb6a131d4e6dd91ffc731687a92
Kevin Pedro <kpedro88@gmail.com>
2018-04-17 02:56:25 -0500

    turn off testnumbering settings for fastsim

--- SimCalorimetry/HcalSimProducers/python/hcalUnsuppressedDigis_cfi.py
+++ SimCalorimetry/HcalSimProducers/python/hcalUnsuppressedDigis_cfi.py
@@ -47,8 +47,9 @@ hcalSimBlock = cms.PSet(
 from Configuration.Eras.Modifier_fastSim_cff import fastSim
 fastSim.toModify( hcalSimBlock, hitsProducer=cms.string('fastSimProducer') )
 
+# test numbering not used in fastsim
 from Configuration.Eras.Modifier_run2_HCAL_2017_cff import run2_HCAL_2017
-run2_HCAL_2017.toModify( hcalSimBlock, TestNumbering = cms.bool(True) )
+(run2_HCAL_2017 & ~fastSim).toModify( hcalSimBlock, TestNumbering = cms.bool(True) )
 
 # remove HE processing for phase 2, completely put in HGCal land
 from Configuration.Eras.Modifier_phase2_hgcal_cff import phase2_hgcal
