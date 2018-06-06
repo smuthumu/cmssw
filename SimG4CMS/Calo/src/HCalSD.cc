95797277f81cc748024b0d2346b14a96ff3f39bd
Kevin Pedro <kpedro88@gmail.com>
2018-06-06 10:10:59 -0500

    downgrade some messages

--- SimG4CMS/Calo/src/HCalSD.cc
+++ SimG4CMS/Calo/src/HCalSD.cc
@@ -561,7 +561,7 @@ void HCalSD::update(const BeginOfJob * job) {
 }
 
 void HCalSD::initRun() {
-  edm::LogWarning("HcalSim") << "InitRun for HFFFFF ";
+  edm::LogInfo("HcalSim") << "InitRun for HFFFFF ";
   if (showerLibrary.get()) showerLibrary.get()->initRun(nullptr, hcalConstants);
   if (showerParam.get())   showerParam.get()->initRun(hcalConstants);
   if (hfshower.get())      hfshower.get()->initRun(hcalConstants);
